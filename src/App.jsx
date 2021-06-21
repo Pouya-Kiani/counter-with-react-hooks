28333c84849011289ff50731adada76eacdb70a0
huhu <p.kiani@live.com>
2021-06-22 00:43:40 +0430

    modify app, add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,86 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useCallback, useState } from 'react';
+// custom hooks
+import useCounter from './hooks/useCounter';
+import useError from './hooks/useError';
+import useCounterInterval from './hooks/useCounterInterval';
+// components
+import Input from './components/Input';
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+
+const App = React.memo(() => {
+  const [isRunning, setIsRunning] = useState(false);
+  const { intervalValue, increase, decrease, setValue } = useCounterInterval();
+  const { counterValue, resetCounter } = useCounter(intervalValue, isRunning);
+  const { error, setError, touched } = useError();
+
+  const handleFieldBlur = useCallback(
+    ({ target }) => {
+      const value = parseInt(target.value, 10);
+      if (!value || value <= 0) {
+        setError('Invalid input...');
+        return;
+      }
+      setValue(value);
+    },
+    [setError, setValue]
+  );
+
+  const handleReset = useCallback(() => {
+    setIsRunning(false);
+    resetCounter();
+  }, [resetCounter]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+      <div className="app-wrapper">
+        <h1 className="counter-value">{counterValue}</h1>
+        <div className="counter-interval-wrapper">
+          <Button
+            className="in-form decrease"
+            name="decrease"
+            label="-"
+            handleClick={() => decrease()}
+          />
+          <Input
+            id="input-value"
+            className="counter-interval-input"
+            label="Counter Interval"
+            error={error}
+            touched={touched}
+            value={intervalValue}
+            onBlur={handleFieldBlur}
+          />
+          <Button
+            className="in-form increase"
+            name="increase"
+            label="+"
+            handleClick={() => increase()}
+          />
+        </div>
+        <div className="action-buttons-wrapper">
+          <button
+            className="action-button start"
+            type="button"
+            name="start-stop"
+            onClick={() => setIsRunning(!isRunning)}
+          >
+            {isRunning ? 'stop' : 'start'}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            className="action-button reset"
+            disabled={counterValue === 0}
+            type="button"
+            name="reset"
+            onClick={() => handleReset()}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+            reset
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+});
 
-export default App
+export default App;
